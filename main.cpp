ac8098e35b0cd31d3aabf4f2eb19af0e87842cf0
ABulger01 <Nyesha2001@gmail.com>
2025-04-11 17:38:25 +0000

    added the loop if the users enters and invalid number

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int numberOfOrganisms;
     int daysMultiply;
     double avgDailyincrease;
 
-  
-  
+    cout << "Enter the starting number of organisms: ";
+    cin >> numberOfOrganisms;
+  if ( numberOfOrganisms < 2) {
+    cout << "Invalid input. The starting number of organisms must be at least 2." << endl;
+    cout << "Enter the starting number of organisms: ";
+    cin >> numberOfOrganisms;
+  }
+    cout << "Enter the average daily increase: ";
+    cin >> avgDailyincrease;
+  if ( avgDailyincrease < 0)
   
