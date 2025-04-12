5c1ff80e58eea4c6c71719f458e8df2683ffa472
ABulger01 <Nyesha2001@gmail.com>
2025-04-12 00:48:48 +0000

    adding in sentences

--- main.cpp
+++ main.cpp
@@ -8,5 +8,21 @@ ifstream inputFile("LineUp.txt");  // Open the file
         return 1;
     }
     string name;
-    sting firstName;
+    string firstName;
+    int count = 0;
   
+  // Read each name from the file and find the first name in alphabetical order
+    while (inputFile >> name) {
+        if (count == 0) {
+            firstName = name;
+        } else {
+            if (name < firstName)
+                firstName = name;
+        }
+        count++;
+    }
+    inputFile.close();
+    cout << "The student at the front of the line is: " << firstName
+  
+  
+    }
