cc4b95c1703a2dceb6407b542c91bf0ea5aecdd1
ABulger01 <Nyesha2001@gmail.com>
2025-04-12 02:25:46 +0000

    adding file

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -7,6 +7,17 @@ int main() {
     
     const int amountperyear = 1000.;
     const int years = 20;
+
+    string fileName = "people.txt.;"
+    ifstream file(fileName);
+
+    infile.open(fileName);
+    if (infile.is_open()) {
+        cout << "File opened successfully." << endl;
+      
+        
+    }
+
     
     
     
