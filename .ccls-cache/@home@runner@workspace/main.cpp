88c56a37b8036b9bcd19297714b28410dc68187a
ABulger01 <Nyesha2001@gmail.com>
2025-04-12 00:25:30 +0000

    added file to program

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,15 +1,12 @@
-
 #include <iostream>
-#include <iomanip>
 #include <fstream>
-#include <string>
 using namespace std;
-
 int main() {
-    ifstream InFile("LineUp.txt");
-  string name;
-// Check if file opened successfully
-  if (!InFile) {
-    cout << "Error opening file" << endl;
-  }
-    
+ifstream inputFile("LineUp.txt");  // Open the file
+    if (!inputFile) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+    string name;
+    sting firstName;
+  
