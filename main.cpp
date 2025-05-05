1f006da4530ab1e4554f52b8ed79a06cc93ad2f0
ABulger01 <Nyesha2001@gmail.com>
2025-05-05 15:38:26 +0000

    adding symbols to represent pattern A

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main() {
   // Pattern A Loop
   cout << "Pattern A" << endl;
-  
+  for (int i = 1; i <= 10; ++i) {
+    for (int j = 1; j <= i; ++j) {
+      cout << "+";
+      
+    }
+  }
     
     
     
