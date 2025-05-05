ca31c3c6a9bfc86f9b3a67973e8b1d56fe333029
ABulger01 <Nyesha2001@gmail.com>
2025-05-05 15:43:21 +0000

    adding symbols to represent pattern B

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -17,7 +17,13 @@ int main() {
   }
 
   // Pattern B Loop
-  cout << "Pattern B" << endl;
+  cout << "Pattern B" << endl
+  for (int i = 10; i >= 1; --i) {
+    for (int j = 1; j <= i; ++j) {
+      cout << "+";
+      
+    }
+  }
     
     
     
