766a22ea7eec7b629bd132d9740f4092bf5f756a
ABulger01 <Nyesha2001@gmail.com>
2025-05-05 15:04:01 +0000

    Add program description

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
+// This program generates a random number and asks the user to guess what the number is. If the user's guess is higher than the number the program will say "Too high!" and if the user's guess is lower than the number the program will say "Too Low!" The user will get a certain amount of guesses.
 #include <iostream>
-#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 int main() {
   { 
     
-    const int amountperyear = 1000.;
-    const int years = 20;
-
-    string fileName = "people.txt.;"
-    ifstream file(fileName);
-
-    infile.open(fileName);
-    if (infile.is_open()) {
-        cout << "File opened successfully." << endl;
-      
-        
-    }
-
-    
-    
-    
-  }
+   
